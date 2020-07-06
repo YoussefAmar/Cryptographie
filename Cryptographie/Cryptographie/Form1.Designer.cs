052e9e2fa8c25460dfcff23969e9176d7628b301
dayxi <57746719+dayxi@users.noreply.github.com>
2020-07-06 19:28:34 +0200

    Add files via upload

---
+++ Cryptographie/Cryptographie/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Cryptographie
+{
+    partial class FEncryptage
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbEncrypt = new System.Windows.Forms.TextBox();
+            this.tbTexte = new System.Windows.Forms.TextBox();
+            this.bCryptPerso = new System.Windows.Forms.Button();
+            this.bCryptC = new System.Windows.Forms.Button();
+            this.lEncrypt = new System.Windows.Forms.Label();
+            this.lValue = new System.Windows.Forms.Label();
+            this.lClé = new System.Windows.Forms.Label();
+            this.lNomClé = new System.Windows.Forms.Label();
+            this.bClé = new System.Windows.Forms.Button();
+            this.tbDecrypt = new System.Windows.Forms.TextBox();
+            this.lDecrypt = new System.Windows.Forms.Label();
+            this.bDecryptC = new System.Windows.Forms.Button();
+            this.bDecryptPerso = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbEncrypt
+            // 
+            this.tbEncrypt.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEncrypt.Location = new System.Drawing.Point(197, 365);
+            this.tbEncrypt.Multiline = true;
+            this.tbEncrypt.Name = "tbEncrypt";
+            this.tbEncrypt.Size = new System.Drawing.Size(664, 109);
+            this.tbEncrypt.TabIndex = 1;
+            // 
+            // tbTexte
+            // 
+            this.tbTexte.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTexte.Location = new System.Drawing.Point(197, 51);
+            this.tbTexte.Multiline = true;
+            this.tbTexte.Name = "tbTexte";
+            this.tbTexte.Size = new System.Drawing.Size(664, 153);
+            this.tbTexte.TabIndex = 2;
+            // 
+            // bCryptPerso
+            // 
+            this.bCryptPerso.BackColor = System.Drawing.Color.Red;
+            this.bCryptPerso.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCryptPerso.Location = new System.Drawing.Point(910, 376);
+            this.bCryptPerso.Name = "bCryptPerso";
+            this.bCryptPerso.Size = new System.Drawing.Size(187, 110);
+            this.bCryptPerso.TabIndex = 3;
+            this.bCryptPerso.Text = "Encryptage";
+            this.bCryptPerso.UseVisualStyleBackColor = false;
+            this.bCryptPerso.Click += new System.EventHandler(this.bCryptPerso_Click);
+            // 
+            // bCryptC
+            // 
+            this.bCryptC.BackColor = System.Drawing.SystemColors.Highlight;
+            this.bCryptC.Font = new System.Drawing.Font("Calibri", 14F);
+            this.bCryptC.Location = new System.Drawing.Point(910, 53);
+            this.bCryptC.Name = "bCryptC";
+            this.bCryptC.Size = new System.Drawing.Size(187, 110);
+            this.bCryptC.TabIndex = 4;
+            this.bCryptC.Text = "Encryptage C#";
+            this.bCryptC.UseVisualStyleBackColor = false;
+            this.bCryptC.Click += new System.EventHandler(this.bCryptC_Click);
+            // 
+            // lEncrypt
+            // 
+            this.lEncrypt.AutoSize = true;
+            this.lEncrypt.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lEncrypt.Location = new System.Drawing.Point(12, 398);
+            this.lEncrypt.Name = "lEncrypt";
+            this.lEncrypt.Size = new System.Drawing.Size(149, 39);
+            this.lEncrypt.TabIndex = 7;
+            this.lEncrypt.Text = "Encrypté :";
+            // 
+            // lValue
+            // 
+            this.lValue.AutoSize = true;
+            this.lValue.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lValue.Location = new System.Drawing.Point(48, 124);
+            this.lValue.Name = "lValue";
+            this.lValue.Size = new System.Drawing.Size(104, 39);
+            this.lValue.TabIndex = 8;
+            this.lValue.Text = "Texte :";
+            this.lValue.Click += new System.EventHandler(this.lValue_Click);
+            // 
+            // lClé
+            // 
+            this.lClé.AutoSize = true;
+            this.lClé.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lClé.Location = new System.Drawing.Point(346, 268);
+            this.lClé.Name = "lClé";
+            this.lClé.Size = new System.Drawing.Size(0, 35);
+            this.lClé.TabIndex = 10;
+            // 
+            // lNomClé
+            // 
+            this.lNomClé.AutoSize = true;
+            this.lNomClé.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNomClé.Location = new System.Drawing.Point(236, 264);
+            this.lNomClé.Name = "lNomClé";
+            this.lNomClé.Size = new System.Drawing.Size(74, 39);
+            this.lNomClé.TabIndex = 11;
+            this.lNomClé.Text = "Clé :";
+            // 
+            // bClé
+            // 
+            this.bClé.BackColor = System.Drawing.Color.Red;
+            this.bClé.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bClé.Location = new System.Drawing.Point(554, 230);
+            this.bClé.Name = "bClé";
+            this.bClé.Size = new System.Drawing.Size(187, 110);
+            this.bClé.TabIndex = 12;
+            this.bClé.Text = "Générer une clé";
+            this.bClé.UseVisualStyleBackColor = false;
+            this.bClé.Click += new System.EventHandler(this.bClé_Click);
+            // 
+            // tbDecrypt
+            // 
+            this.tbDecrypt.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDecrypt.Location = new System.Drawing.Point(197, 526);
+            this.tbDecrypt.Multiline = true;
+            this.tbDecrypt.Name = "tbDecrypt";
+            this.tbDecrypt.Size = new System.Drawing.Size(664, 109);
+            this.tbDecrypt.TabIndex = 13;
+            // 
+            // lDecrypt
+            // 
+            this.lDecrypt.AutoSize = true;
+            this.lDecrypt.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lDecrypt.Location = new System.Drawing.Point(12, 566);
+            this.lDecrypt.Name = "lDecrypt";
+            this.lDecrypt.Size = new System.Drawing.Size(152, 39);
+            this.lDecrypt.TabIndex = 14;
+            this.lDecrypt.Text = "Décrypté :";
+            this.lDecrypt.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // bDecryptC
+            // 
+            this.bDecryptC.BackColor = System.Drawing.SystemColors.Highlight;
+            this.bDecryptC.Font = new System.Drawing.Font("Calibri", 14F);
+            this.bDecryptC.Location = new System.Drawing.Point(910, 208);
+            this.bDecryptC.Name = "bDecryptC";
+            this.bDecryptC.Size = new System.Drawing.Size(187, 110);
+            this.bDecryptC.TabIndex = 15;
+            this.bDecryptC.Text = "Decryptage C#";
+            this.bDecryptC.UseVisualStyleBackColor = false;
+            this.bDecryptC.Click += new System.EventHandler(this.bDecryptC_Click);
+            // 
+            // bDecryptPerso
+            // 
+            this.bDecryptPerso.BackColor = System.Drawing.Color.Red;
+            this.bDecryptPerso.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bDecryptPerso.Location = new System.Drawing.Point(910, 531);
+            this.bDecryptPerso.Name = "bDecryptPerso";
+            this.bDecryptPerso.Size = new System.Drawing.Size(187, 110);
+            this.bDecryptPerso.TabIndex = 16;
+            this.bDecryptPerso.Text = "Decryptage";
+            this.bDecryptPerso.UseVisualStyleBackColor = false;
+            this.bDecryptPerso.Click += new System.EventHandler(this.bDecryptPerso_Click);
+            // 
+            // FEncryptage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(1140, 679);
+            this.Controls.Add(this.bDecryptPerso);
+            this.Controls.Add(this.bDecryptC);
+            this.Controls.Add(this.lDecrypt);
+            this.Controls.Add(this.tbDecrypt);
+            this.Controls.Add(this.bClé);
+            this.Controls.Add(this.lNomClé);
+            this.Controls.Add(this.lClé);
+            this.Controls.Add(this.lValue);
+            this.Controls.Add(this.lEncrypt);
+            this.Controls.Add(this.bCryptC);
+            this.Controls.Add(this.bCryptPerso);
+            this.Controls.Add(this.tbTexte);
+            this.Controls.Add(this.tbEncrypt);
+            this.Font = new System.Drawing.Font("MS Reference Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "FEncryptage";
+            this.Text = "Application d\'encryptage";
+            this.Load += new System.EventHandler(this.FEncryptage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbEncrypt;
+        private System.Windows.Forms.TextBox tbTexte;
+        private System.Windows.Forms.Button bCryptPerso;
+        private System.Windows.Forms.Button bCryptC;
+        private System.Windows.Forms.Label lEncrypt;
+        private System.Windows.Forms.Label lValue;
+        private System.Windows.Forms.Label lClé;
+        private System.Windows.Forms.Label lNomClé;
+        private System.Windows.Forms.Button bClé;
+        private System.Windows.Forms.TextBox tbDecrypt;
+        private System.Windows.Forms.Label lDecrypt;
+        private System.Windows.Forms.Button bDecryptC;
+        private System.Windows.Forms.Button bDecryptPerso;
+    }
+}
+
---
+++ Cryptographie/Cryptographie/Form1.cs
@@ -0,0 +1,506 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace Cryptographie
+{
+
+    public partial class FEncryptage : Form
+    {
+       
+        #region Cryptage/DecryptagePerso
+
+        #region Clé
+
+        private void bClé_Click(object sender, EventArgs e)
+        {
+                Random r = new Random();
+
+                long i = r.Next(2,73);
+
+                lClé.Text = i.ToString();
+        }
+
+        #endregion
+
+        #region Variable globale
+        static class Global
+        {
+            private static ulong[] valeur = new ulong[1000];
+
+            public static ulong[] GlobalVar
+            {
+                get { return valeur; }
+                set { valeur = value; }
+            }
+        }
+        #endregion
+
+        #region CryptagePerso
+
+        private void bCryptPerso_Click(object sender, EventArgs e)
+        {
+            if (lClé.Text == "")
+            {
+                tbEncrypt.Text = "Veuillez générer une clé avant d'encrypter votre message !";
+            }
+
+           else if (tbTexte.Text == "")
+            {
+                tbEncrypt.Text = "Veuillez écrire un message dans la section Texte !";
+            }
+           
+            else
+            {
+                int i = 0;
+
+                int Clé = int.Parse(lClé.Text);
+
+                ulong[] value = Cryptage(tbTexte.Text, Clé,i);
+
+                Global.GlobalVar = value;
+
+                string myStringOutput = String.Join("", value.Select(p => p.ToString()).ToArray());
+
+                tbEncrypt.Text = myStringOutput;
+
+            }
+        }
+
+         public ulong[] Cryptage(string texte, int code, int i)
+        {
+            char[] lettre = texte.ToCharArray();
+
+            ulong[] crypt = new ulong[1000];
+
+            for(i = 0; i < lettre.Length; i++)
+            {
+
+            switch(lettre[i])
+                {
+                    case 'A':
+                    case 'a':
+                    case 'à':
+                        crypt[i] = Convert.ToUInt64(code + 13);
+                        break;
+
+                    case 'B':
+                    case 'b':
+                        crypt[i] = Convert.ToUInt64(code + 8);
+                        break;
+
+                    case 'C':
+                    case 'c':
+                        crypt[i] = Convert.ToUInt64(code + 20);
+                        break;
+
+                    case 'D':
+                    case 'd':
+                        crypt[i] = Convert.ToUInt64(code + 2);
+                        break;
+                    case 'E':
+                    case 'e':
+                    case 'é':
+                    case 'è':
+                    case 'ê':
+                        crypt[i] = Convert.ToUInt64(code + 23);
+                        break;
+
+                    case 'F':
+                    case 'f':
+                        crypt[i] = Convert.ToUInt64(code + 26);
+                        break;
+
+                    case 'g':
+                    case 'G':
+                        crypt[i] = Convert.ToUInt64(code + 4);
+                        break;
+
+                    case 'H':
+                    case 'h':
+                        crypt[i] = Convert.ToUInt64(code + 6);
+                        break;
+
+                    case 'I':
+                    case 'i':
+                        crypt[i] = Convert.ToUInt64(code + 18);
+                        break;
+
+                    case 'J':
+                    case 'j':
+                        crypt[i] = Convert.ToUInt64(code + 22);
+                        break;
+
+                    case 'K':
+                    case 'k':
+                        crypt[i] = Convert.ToUInt64(code + 5);
+                        break;
+
+                    case 'L':
+                    case 'l':
+                        crypt[i] = Convert.ToUInt64(code + 15);
+                        break;
+
+                    case 'M':
+                    case 'm':
+                        crypt[i] = Convert.ToUInt64(code + 19);
+                        break;
+
+                    case 'N':
+                    case 'n':
+                        crypt[i] = Convert.ToUInt64(code + 10);
+                        break;
+
+                    case 'O':
+                    case 'o':
+                        crypt[i] = Convert.ToUInt64(code + 7);
+                        break;
+
+                    case 'P':
+                    case 'p':
+                        crypt[i] = Convert.ToUInt64(code + 25);
+                        break;
+
+                    case 'Q':
+                    case 'q':
+                        crypt[i] = Convert.ToUInt64(code + 3);
+                        break;
+
+                    case 'R':
+                    case 'r':
+                        crypt[i] = Convert.ToUInt64(code + 9);
+                        break;
+
+                    case 'S':
+                    case 's':
+                        crypt[i] = Convert.ToUInt64(code + 11);
+                        break;
+
+                    case 'T':
+                    case 't':
+                        crypt[i] = Convert.ToUInt64(code + 14);
+                        break;
+
+                    case 'U':
+                    case 'u':
+                    case 'ù':
+                        crypt[i] = Convert.ToUInt64(code + 24);
+                        break;
+
+                    case 'V':
+                    case 'v':
+                        crypt[i] = Convert.ToUInt64(code + 16);
+                        break;
+
+                    case 'W':
+                    case 'w':
+                        crypt[i] = Convert.ToUInt64(code + 21);
+                        break;
+
+                    case 'X':
+                    case 'x':
+                        crypt[i] = Convert.ToUInt64(code + 17);
+                        break;
+
+                    case 'Y':
+                    case 'y':
+                        crypt[i] = Convert.ToUInt64(code + 1);
+                        break;
+
+                    case 'Z':
+                    case 'z':
+                        crypt[i] = Convert.ToUInt64(code + 12);
+                        break;
+
+                    default: crypt[i] = Convert.ToUInt64(00); break;
+
+                }
+
+            }
+
+            return crypt;
+        }
+
+        #endregion
+
+        #region DecrytagePerso
+
+        private void bDecryptPerso_Click(object sender, EventArgs e)
+        {
+            if (lClé.Text == "")
+            {
+                tbDecrypt.Text = "Veuillez générer une clé avant d'encrypter votre message !";
+            }
+
+            else if (tbTexte.Text == "")
+            {
+                tbDecrypt.Text = "Veuillez écrire un message dans la section Texte !";
+            }
+
+            else if (tbEncrypt.Text == "")
+            {
+                tbDecrypt.Text = "Veuillez crypter le message avant de le decrypter !";
+            }
+
+            else
+            {
+
+                ulong[] value = new ulong[1000];
+
+                int Clé = int.Parse(lClé.Text);
+
+                value = Global.GlobalVar;
+
+                string[] traduction = Decryptage(value, Clé);
+
+                string result = String.Concat(traduction);
+
+                tbDecrypt.Text = result;
+            }
+        }
+
+        public string[] Decryptage(ulong[] value, int code)
+        {
+            string[] texte = new string[1000];
+
+            int nombre_codé;
+
+            for (int j = 0; j < texte.Length; j++)
+
+            {
+
+                nombre_codé = Convert.ToInt32(value[j]);
+
+                if(nombre_codé != 0)
+
+                nombre_codé = nombre_codé - code;
+
+                switch (nombre_codé)
+                {
+
+                    case 13:
+                        texte[j] = "a";
+                        break;
+
+
+                    case 8:
+                        texte[j] = "b";
+                        break;
+
+
+                    case 20:
+                        texte[j] = "c";
+                        break;
+
+
+                    case 2:
+                        texte[j] = "d";
+                        break;
+
+
+                    case 23:
+                        texte[j] = "e";
+                        break;
+
+             
+                    case 26 :
+                        texte[j] = "f";
+                        break;
+
+                
+                    case 4 :
+                        texte[j] = "g";
+                        break;
+
+                  
+                    case 6 :
+                        texte[j] = "h";
+                        break;
+
+               
+                    case 18 :
+                        texte[j] = "i";
+                        break;
+
+              
+                    case 22 :
+                        texte[j] = "j";
+                        break;
+
+                  
+                    case 5 :
+                        texte[j] = "k";
+                        break;
+
+                   
+                    case 15 :
+                        texte[j] = "l";
+                        break;
+
+                    
+                    case 19 :
+                        texte[j] = "m";
+                        break;
+
+                 
+                    case 10 :
+                        texte[j] = "n";
+                        break;
+
+                 
+                    case 7 :
+                        texte[j] = "o";
+                        break;
+
+                  
+                    case 25 :
+                        texte[j] = "p";
+                        break;
+
+               
+                    case 3 :
+                        texte[j] = "q";
+                        break;
+
+              
+                    case 9 :
+                        texte[j] = "r";
+                        break;
+
+                 
+                    case 11 :
+                        texte[j] = "s";
+                        break;
+
+                 
+                    case 14 :
+                        texte[j] = "t";
+                        break;
+
+               
+                    case 24 :
+                        texte[j] = "u";
+                        break;
+
+                
+                    case 16 :
+                        texte[j] = "v";
+                        break;
+
+             
+                    case 21:
+                        texte[j] = "w";
+                        break;
+
+                  
+                    case 17 :
+                        texte[j] = "x";
+                        break;
+
+               
+                    case 1 :
+                        texte[j] = "y";
+                        break;
+
+                 
+                    case 12:
+                        texte[j] = "z";
+                        break;
+
+                    default : texte[j] = " "; break;
+
+                }
+
+            }
+
+            return texte;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region CryptageLogiciel
+
+        #region Encryptage
+        private void bCryptC_Click(object sender, EventArgs e)
+        {
+
+            string hash = "O0eh3P";
+
+                byte[] texte_envoi = UTF8Encoding.UTF8.GetBytes(tbTexte.Text);
+                using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+                {
+                    byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider()
+                { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                    {
+                        ICryptoTransform transform = tripDes.CreateEncryptor();
+                        byte[] results = transform.TransformFinalBlock(texte_envoi, 0, texte_envoi.Length);
+                        tbEncrypt.Text = Convert.ToBase64String(results, 0, results.Length);
+                    }
+                }
+        }
+
+        #endregion
+
+        #region Decryptage
+        private void bDecryptC_Click(object sender, EventArgs e)
+        {
+            string hash = "O0eh3P";
+
+            byte[] texte_recu = Convert.FromBase64String(tbEncrypt.Text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider()
+                { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripDes.CreateDecryptor();
+                    byte[] results = transform.TransformFinalBlock(texte_recu, 0, texte_recu.Length);
+                    tbDecrypt.Text = UTF8Encoding.UTF8.GetString(results);
+                }
+
+            }
+
+        }
+
+        #endregion
+
+        #endregion
+
+        #region inutile
+
+        public FEncryptage()
+        {
+            InitializeComponent();
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+         private void lValue_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FEncryptage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        #endregion
+
+    }
+}
